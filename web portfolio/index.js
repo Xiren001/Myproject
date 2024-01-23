aa7d621c14e59e93c19db5a7d84b39abd235a264
Myko Xiren Dungca <mykoxiren992003@gmail.com>
2024-01-23 15:29:42 +0800

    webport

---
+++ web portfolio/index.js
@@ -0,0 +1,24 @@
+
+//coment
+/*
+comment
+*/
+
+// var, let , conts
+
+let age;
+age = 20;
+//let ages = 20;
+
+let name = "xiren";
+let student = false;
+
+console.log(age);
+
+console.log("hello" , name); //use comma
+
+console.log(student);
+
+document.getElementById("p1").innerHTML = "hello" + name;
+document.getElementById("p2").innerHTML = "you are" + age + "yes old";
+document.getElementById("p3").innerHTML = "enrolled" + student;
