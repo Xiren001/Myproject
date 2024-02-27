ac7c901ff8eabeb34ca16615557fdf5cdd70c4c6
Myko Xiren Dungca <mykoxiren992003@gmail.com>
2024-02-28 00:16:02 +0800

    added contact

--- IT26/BARANGAY/barangay.js
+++ IT26/BARANGAY/barangay.js
@@ -1,18 +1,29 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+const wrapper = document.querySelector(".wrapper");
+const wrappers = document.querySelector(".wrappers");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const btnPopupContact = document.querySelector(".btnContact-popup");
+const iconClose = document.querySelector(".icon-close");
+const closes = document.querySelector(".closes");
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-    });
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-    });
- btnPopup.addEventListener('click', ()=> {
-        wrapper.classList.add('active-popup');
-        });
- iconClose.addEventListener('click', ()=> {
-        wrapper.classList.remove('active-popup');
-        });
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+  wrappers.classList.remove("active-popup");
+});
+btnPopupContact.addEventListener("click", () => {
+  wrappers.classList.add("active-popup");
+  wrapper.classList.remove("active-popup");
+});
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
+closes.addEventListener("click", () => {
+  wrappers.classList.remove("active-popup");
+});
